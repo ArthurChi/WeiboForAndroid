5776cccda7fc9fcda8736b27ee1941292930e1d4
cjfire <cjfire@sina.cn>
2016-06-18 10:34:04 +0800

    微博SDK集成完成

--- Weibo2Android/app/src/main/java/com/weibo/cjfire/weibo2android/MainActivity.java
+++ Weibo2Android/app/src/main/java/com/weibo/cjfire/weibo2android/MainActivity.java
@@ -1,13 +1,64 @@
 package com.weibo.cjfire.weibo2android;
 
+import android.provider.SyncStateContract;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String APP_KEY = "3283806218";
+    private static final String REDIRECT_URL = "0993b0ccb43bb38d0acd230477f172d6";
+
+    private static final String SCOPE = "all";
+
+    AuthInfo mAuthInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuthInfo = new AuthInfo(this, MainActivity.APP_KEY, "https://api.weibo.com/oauth2/default.html", MainActivity.SCOPE);
+    }
+
+    public void authBtnDidClicked(View view) {
+        SsoHandler mSsoHandler = new SsoHandler(this, mAuthInfo);
+        mSsoHandler.authorize(new AuthListener());
+    }
+}
+
+class AuthListener implements WeiboAuthListener {
+
+    @Override
+    public void onComplete(Bundle values) {
+        // 从 Bundle 中解析 Token
+        Oauth2AccessToken mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+        if (mAccessToken.isSessionValid()) {
+            // 保存 Token 到 SharedPreferences
+            Log.i("test", values.toString());
+
+        } else {
+            // 当您注册的应用程序签名不正确时，就会收到 Code，请确保签名正确
+            String code = values.getString("code", "");
+
+        }
+    }
+
+    @Override
+    public void onCancel() {
+    }
+
+    @Override
+    public void onWeiboException(WeiboException e) {
     }
 }
