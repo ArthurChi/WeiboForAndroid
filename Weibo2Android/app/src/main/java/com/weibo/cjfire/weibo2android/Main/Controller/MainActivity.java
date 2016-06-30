a721e661328885e1b4ea84c2e7c6f82b70fd6004
VassilyChi <cjfire@sina.cn>
2016-06-30 15:58:37 +0800

    正常工作

--- Weibo2Android/app/src/main/java/com/weibo/cjfire/weibo2android/Main/Controller/MainActivity.java
+++ Weibo2Android/app/src/main/java/com/weibo/cjfire/weibo2android/Main/Controller/MainActivity.java
@@ -3,11 +3,14 @@ package com.weibo.cjfire.weibo2android.Main.Controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
 import com.sina.weibo.sdk.auth.sso.SsoHandler;
-import com.weibo.cjfire.weibo2android.AuthListener;
+import com.sina.weibo.sdk.exception.WeiboException;
 import com.weibo.cjfire.weibo2android.R;
 
 /**
@@ -45,3 +48,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+class AuthListener implements WeiboAuthListener {
+
+    @Override
+        public void onComplete(Bundle values) {
+            // 从 Bundle 中解析 Token
+            Oauth2AccessToken mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+            if (mAccessToken.isSessionValid()) {
+                // 保存 Token 到 SharedPreferences
+                String uid = values.getString("uid");
+                String userName = values.getString("userName");
+                Integer expires = values.getInt("expires_in");
+                String refreshToken = values.getString("refresh_token");
+                String accessToken = values.getString("access_token");
+
+                Log.i("test", accessToken);
+            } else {
+                // 当您注册的应用程序签名不正确时，就会收到 Code，请确保签名正确
+                String code = values.getString("code", "");
+
+            }
+        }
+
+        @Override
+        public void onCancel() {
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+        }
+    }
+
+
